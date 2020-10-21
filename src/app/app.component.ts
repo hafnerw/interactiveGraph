3257ac1d3a9cfc7b9c86a8f76c6322e866ca11ed
Hafnerw <w-hafner@live.de>
2020-10-21 13:59:03 +0200

    grouped paths added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,14 +19,21 @@ export class AppComponent {
   }
 
   switchCurrent(e) {
-    if (e.target.tagName !== 'svg') {
+    console.log(e);
+    if (e.target.tagName !== 'svg' && !e.target.parentElement.classList.contains('parent')) {
       this.currentElem = e.target;
-      this.currentInner = this.sanitizer.bypassSecurityTrustHtml(e.target.outerHTML);
+      this.currentInner = this.safeHtml(e.target.outerHTML);
+    } else if (e.target.parentElement.classList.contains('parent')) {
+      this.currentElem = e.target.parentElement;
+      this.currentInner = this.safeHtml(e.target.parentElement.outerHTML);
     }
   }
 
   getViewBox() {
     return this.currentElem.getBBox().x + ' ' + this.currentElem.getBBox().y + ' '
       + this.currentElem.getBBox().width + ' ' + this.currentElem.getBBox().height;
+  }
+  safeHtml(val): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(val);
   }
 }
